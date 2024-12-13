f07edf782db19fd8937c5ce52380a69f894436fd
Jakub Glinka <jakubglinka9@gmail.com>
2024-12-13 22:02:36 +0100

    Cleanup the code

--- 13/1.py
+++ 13/1.py
@@ -3,24 +3,18 @@ import re
 with open("in") as f:
     ins = f.read().strip().split("\n\n")
 
+
+def get_numbers(s: str):
+    return [int(num) for num in re.findall(r"\d+", s)]
+
+
 ans = 0
 
 for machine in ins:
     a, b, prize = machine.split("\n")
-    match = re.search(r"(?:X)\+(\d+)", a)
-    ax = int(match.groups(1)[0])
-    match = re.search(r"(?:Y)\+(\d+)", a)
-    ay = int(match.groups(1)[0])
-
-    match = re.search(r"(?:X)\+(\d+)", b)
-    bx = int(match.groups(1)[0])
-    match = re.search(r"(?:Y)\+(\d+)", b)
-    by = int(match.groups(1)[0])
-
-    match = re.search(r"(?:X)=(\d+)", prize)
-    prize_x = int(match.groups(1)[0])
-    match = re.search(r"(?:Y)=(\d+)", prize)
-    prize_y = int(match.groups(1)[0])
+    ax, ay = get_numbers(a)
+    bx, by = get_numbers(b)
+    prize_x, prize_y = get_numbers(prize)
 
     min_cost = float("inf")
     for ops in range(202):
@@ -32,7 +26,7 @@ for machine in ins:
             if start_x == prize_x and start_y == prize_y:
                 cost = a_presses * 3 + b_presses
                 min_cost = min(min_cost, cost)
-    
+
     if min_cost != float("inf"):
         ans += min_cost
 
--- 13/2.py
+++ 13/2.py
@@ -6,27 +6,26 @@ with open("in") as f:
 
 ans = 0
 
+
+def get_numbers(s: str):
+    return [int(num) for num in re.findall(r"\d+", s)]
+
+
 for machine in ins:
     a, b, prize = machine.split("\n")
-    match = re.search(r"(?:X)\+(\d+)", a)
-    ax = int(match.groups(1)[0])
-    match = re.search(r"(?:Y)\+(\d+)", a)
-    ay = int(match.groups(1)[0])
-
-    match = re.search(r"(?:X)\+(\d+)", b)
-    bx = int(match.groups(1)[0])
-    match = re.search(r"(?:Y)\+(\d+)", b)
-    by = int(match.groups(1)[0])
-
-    match = re.search(r"(?:X)=(\d+)", prize)
-    prize_x = int(match.groups(1)[0]) + 10000000000000
-    match = re.search(r"(?:Y)=(\d+)", prize)
-    prize_y = int(match.groups(1)[0]) + 10000000000000
+    ax, ay = get_numbers(a)
+    bx, by = get_numbers(b)
+    prize_x, prize_y = get_numbers(prize)
+    prize_x += 10000000000000
+    prize_y += 10000000000000
 
     x, y = np.linalg.solve([(ax, bx), (ay, by)], [(prize_x), (prize_y)])
     a_presses = round(x)
     b_presses = round(y)
-    if a_presses * ax + b_presses * bx == prize_x and a_presses * ay + b_presses * by == prize_y:
+    if (
+        a_presses * ax + b_presses * bx == prize_x
+        and a_presses * ay + b_presses * by == prize_y
+    ):
         ans += 3 * a_presses + b_presses
 
 print(ans)

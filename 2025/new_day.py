f2fa90182dd065de3d1f45f5f5539510f7c1299b
Jakub Glinka <jakubglinka9@gmail.com>
2025-12-02 10:26:41 +0100

    Add script to creating new days file structure

--- 2025/new_day.py
+++ 2025/new_day.py
@@ -16,18 +16,24 @@ if len(new_folder_name) == 1:
 new_folder_path = os.path.join(current_dir, new_folder_name)
 os.makedirs(new_folder_path)
 
-# Create three files in the new folder
-for i in range(1, 3):
-    file_path = os.path.join(new_folder_path, f"{i}.py")
-    with open(file_path, "w") as file:
-        file.write(
-"""with open("in.txt") as f:
-    line = f.read().strip()
+# Create main.go file
+file_path = os.path.join(new_folder_path, "main.go")
+with open(file_path, "w") as file:
+    file.write(
+        """package main
+    
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+}
 """
-        )
+    )
 
 file_path = os.path.join(new_folder_path, "in.txt")
 with open(file_path, "w") as f:
     ...
 
-print(f"Created folder {new_folder_name} with files 1.py, 2.py, in.txt")
+print("Done!")
